a1333c2ac94f26975be42e74c479b5ed6b3a3b12
drrakendu78 <lange.noire7878@gmail.com>
2024-01-10 17:41:13 +0100

    v2.3

--- TradSCinstallerauto.bat
+++ TradSCinstallerauto.bat
@@ -4,33 +4,67 @@ setlocal enabledelayedexpansion
 :: Liste des lecteurs où nous allons rechercher StarCitizen
 set "drives=C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 
-:: Parcourir chaque lecteur pour trouver le répertoire StarCitizen
+echo Recherche du repertoire StarCitizen dans les dossiers specifiques de chaque lecteur...
+
+:: Parcourir chaque lecteur
 for %%d in (%drives%) do (
-    if exist "%%d:\Program Files\Roberts Space Industries\StarCitizen" (
-        set "starCitizenPath=%%d:\Program Files\Roberts Space Industries\StarCitizen"
-        goto foundPath
+    if exist "%%d:\StarCitizen" (
+        set "starCitizenPath=%%d:\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\Roberts Space Industries\StarCitizen" (
+        set "starCitizenPath=%%d:\Roberts Space Industries\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\jeux\StarCitizen" (
+        set "starCitizenPath=%%d:\jeux\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\game\StarCitizen" (
+        set "starCitizenPath=%%d:\game\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\games\StarCitizen" (
+        set "starCitizenPath=%%d:\games\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\games\Roberts Space Industries\StarCitizen" (
+        set "starCitizenPath=%%d:\games\Roberts Space Industries\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\game\Roberts Space Industries\StarCitizen" (
+        set "starCitizenPath=%%d:\game\Roberts Space Industries\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\jeu\Roberts Space Industries\StarCitizen" (
+        set "starCitizenPath=%%d:\jeu\Roberts Space Industries\StarCitizen"
+        goto :found
+    )
+    if exist "%%d:\jeux\Roberts Space Industries\StarCitizen" (
+        set "starCitizenPath=%%d:\jeux\Roberts Space Industries\StarCitizen"
+        goto :found
     )
 )
 
-:: Si StarCitizen n'est pas trouvé sur tous les lecteurs
+:: Si StarCitizen n'est pas trouve sur tous les lecteurs
 echo Le répertoire StarCitizen n'a pas ete trouve sur les lecteurs disponibles.
 pause
 exit /b
 
-:foundPath
-echo Le répertoire StarCitizen a ete trouvé à l'emplacement: %starCitizenPath%
+:found
+echo Le repertoire StarCitizen a ete trouvé à l'emplacement: !starCitizenPath!
 
-:: En train de telecharger le script depuis GitHub
+:: En train de télécharger le script depuis GitHub
 echo Telechargement du script depuis GitHub...
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/drrakendu78/TradSC/main/TradSC_Updater.ps1' -OutFile '%TEMP%\TradSC_Updater.ps1'"
 
-:: Déplacez le script téléchargé vers le répertoire StarCitizen
-echo Deplacement du script vers le répertoire StarCitizen...
-move /Y "%TEMP%\TradSC_Updater.ps1" "%starCitizenPath%"
+:: Deplacer le script telecharge vers le répertoire StarCitizen
+echo Déplacement du script vers le repertoire StarCitizen...
+move /Y "%TEMP%\TradSC_Updater.ps1" "!starCitizenPath!"
 
-:: Exécutez le script en arrière-plan
+:: Executer le script en arrière-plan
 echo Execution du script en arrière-plan...
-powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ""%starCitizenPath%\TradSC_Updater.ps1""' -Verb RunAs"
+powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ""!starCitizenPath!\TradSC_Updater.ps1""' -Verb RunAs"
 
-:: Fermez le fichier batch
+:: Fermer le fichier batch
 exit
