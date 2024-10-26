a840ff29c6975b5f72c2c6325393791a499a69b7
drrakendu78 <lange.noire7878@gmail.com>
2024-10-26 19:17:58 +0200

    Update TradSCinstallerauto.bat

--- TradSCinstallerauto.bat
+++ TradSCinstallerauto.bat
@@ -1,8 +1,8 @@
 @echo off
-:: Verifiez si le script est execute en tant qu'administrateur
+:: Vérifiez si le script est exécuté en tant qu'administrateur
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' == '0' (
-    echo Script execute avec des droits d'administrateur
+    echo Script exécuté avec des droits d'administrateur
 ) else (
     echo Relancez ce script en tant qu'administrateur...
     powershell -Command "Start-Process '%~0' -Verb RunAs"
@@ -12,12 +12,12 @@ if '%errorlevel%' == '0' (
 :: Code principal
 setlocal enabledelayedexpansion
 
-:: Liste des lecteurs a verifier
+:: Liste des lecteurs à vérifier
 set "drives=C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 
 :menu
 cls
-echo Recherche du repertoire StarCitizen...
+echo Recherche du répertoire StarCitizen...
 
 :: Initialisation des variables
 set "foundPaths="
@@ -46,10 +46,10 @@ for %%d in (%drives%) do (
     )
 )
 
-:: Demander a l'utilisateur de choisir un chemin
-set /p choice="Entrez le numero du chemin : "
+:: Demander à l'utilisateur de choisir un chemin
+set /p choice="Entrez le numéro du chemin : "
 
-:: Extraire le chemin selectionne
+:: Extraire le chemin sélectionné
 set "selectedPath="
 for /f "tokens=%choice% delims=;" %%a in ("!foundPaths!") do set "selectedPath=%%a"
 
@@ -60,19 +60,19 @@ if "%selectedPath%"=="" (
     goto menu
 )
 
-echo Le repertoire StarCitizen a ete trouve a l'emplacement: %selectedPath%!
+echo Le répertoire StarCitizen a été trouvé à l'emplacement: %selectedPath%!
 
-:: Telechargement du script depuis GitHub
-echo Telechargement du script depuis GitHub...
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/drrakendu78/TradSC/main/TradSC_Updater.ps1' -OutFile '%TEMP%\TradSC_Updater.ps1'"
+:: Téléchargement de l'exécutable depuis GitHub
+echo Téléchargement de l'exécutable depuis GitHub...
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/drrakendu78/TradSC/raw/refs/heads/main/TradSC_Updater.exe' -OutFile '%TEMP%\TradSC_Updater.exe'"
 
-:: Deplacement et remplacement du script telecharge
-echo Deplacement et remplacement du script vers le repertoire StarCitizen...
-move /Y "%TEMP%\TradSC_Updater.ps1" "%selectedPath%"
+:: Déplacement et remplacement de l'exécutable téléchargé
+echo Déplacement et remplacement de l'exécutable vers le répertoire StarCitizen...
+move /Y "%TEMP%\TradSC_Updater.exe" "%selectedPath%"
 
-:: Execution du script en arriere-plan
-echo Execution du script en arriere-plan...
-powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ""%selectedPath%\TradSC_Updater.ps1""' -Verb RunAs"
+:: Exécution de l'exécutable
+echo Exécution de l'exécutable...
+start "" "%selectedPath%\TradSC_Updater.exe"
 
 :: Fermeture du fichier batch
 exit
