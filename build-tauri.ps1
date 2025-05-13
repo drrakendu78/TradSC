30035b00f4f1e8266e79998826a179eb1cb1b3a9
drrakendu78 <lange.noire7878@gmail.com>
2025-05-13 21:11:28 +0200

    Delete build-tauri.ps1

--- build-tauri.ps1
+++
@@ -1,9 +0,0 @@
-# Chemin vers ta clé privée
-$privateKeyPath = "$PSScriptRoot\~\.tauri\key.key"
-
-if (Test-Path $privateKeyPath) {
-    $env:TAURI_PRIVATE_KEY = Get-Content $privateKeyPath -Raw
-    pnpm tauri build
-} else {
-    Write-Host "Erreur : Clé privée non trouvée à $privateKeyPath"
-}
