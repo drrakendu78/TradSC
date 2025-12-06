e8542a98e44d7eb210c9beafe154b4a7fae37943
drrakendu78 <lange.noire7878@gmail.com>
2025-12-06 22:10:58 +0100

    fix

--- src/components/custom/control-menu.tsx
+++ src/components/custom/control-menu.tsx
@@ -8,13 +8,13 @@ export default function ControlMenu() {
     const close = async () => await appWindow?.close();
 
     return (
-        <div className='flex flex-row gap-2 absolute right-4 top-4 z-10 items-center'>
+        <div className='flex flex-row gap-2 fixed right-4 top-4 z-[100] items-center pointer-events-auto'>
             <button onClick={minimize} 
-                className='bg-gray-500 hover:bg-yellow-400 rounded-full flex justify-center items-center p-0.5 group shrink-0'>
+                className='bg-gray-500 hover:bg-yellow-400 rounded-full flex justify-center items-center p-0.5 group shrink-0 pointer-events-auto relative z-[100]'>
                 <Minus strokeWidth={2} className='h-2.5 w-2.5 text-gray-100 group-hover:text-background' />
             </button>
             <button onClick={close} 
-                className='bg-gray-500 hover:bg-red-400 rounded-full flex justify-center items-center p-0.5 group shrink-0'>
+                className='bg-gray-500 hover:bg-red-400 rounded-full flex justify-center items-center p-0.5 group shrink-0 pointer-events-auto relative z-[100]'>
                 <X strokeWidth={2} className='h-2.5 w-2.5 text-gray-100 group-hover:text-background'/>
             </button>
         </div>
